41e23fffb83018e7b539acf23f00dd3a8bcff7ff
wyjsuper <63230878+wyjsuper@users.noreply.github.com>
2020-04-06 17:04:13 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyARgs4t4g1bbS5bPvHevjwsL2KL3enUK5M');
 define('GJ_CODE', 'US');
 define('SITE_NAME', 'y2b');
 define('TITLENAME', 'y2b');
 define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
+define('EMAIL', '621005591@qq.com');
 define('NAME', 'admin');
 define('PASSWORD', 'admin');
 define('LOGINHTML', '<!DOCTYPE html>
